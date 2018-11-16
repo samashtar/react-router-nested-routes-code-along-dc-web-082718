7b50b9616add40faf91155e3bbdfa51bcc095d0e
samashtar <sam.ashtar@flatironschool.com>
2018-11-15 21:05:51 -0500

    changes

---
+++ src/containers/MovieShow.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MovieShow = props => {
+  return (
+    <div>
+      <ul>
+        <Provider>df lol</Provider> <li>lol</li>
+      </ul>
+      <div />
+    </div>
+  );
+};
+
+export default MovieShow;
